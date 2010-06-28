a8e8a4dc1cb0afca4aee83a3730ad574c1e54f87
a.vanderkolk@gmail.com <a.vanderkolk@gmail.com@92f022ba-f89c-8ad2-c30b-ffff9fb782a9>
2010-06-28 12:18:10 +0000

    * Disabled instanced objects & converted them to normal

--- trunk/refactor/The Incredible Evolving Robot/Objects/Projectile/DoubleBullet.cs
+++ trunk/refactor/The Incredible Evolving Robot/Objects/Projectile/DoubleBullet.cs
@@ -12,18 +12,21 @@ using Tier.Objects.Attachable;
 
 
 namespace Tier.Objects.Destroyable.Projectile
-{  public class DoubleBullet : Tier.Objects.Projectile
-  {
-    public DoubleBullet(Game game, Position sourcePos)
-      : base(game, sourcePos, 30)
+{
+    public class DoubleBullet : Tier.Objects.Projectile
     {
-      this.TimeToLive = 1500;
-      this.ModelName = "DoubleBullet";
-      this.Scale = 0.5f;
-      this.Sort = SortFilter.OtherInstanced;
-      this.IsInstanced = true;
-      this.RotationFix = Matrix.CreateRotationX(-MathHelper.PiOver2);
-      this.Initialize();
+        public DoubleBullet(Game game, Position sourcePos)
+            : base(game, sourcePos, 30)
+        {
+            this.TimeToLive = 1500;
+            this.ModelName = "DoubleBullet";
+            this.ModelMeta = TierGame.ContentHandler.GetModelMeta(this.ModelName);
+            this.Model = this.ModelMeta.Model;
+            this.Scale = 0.5f;
+            //this.Sort = SortFilter.OtherInstanced;
+            //this.IsInstanced = true;
+            this.RotationFix = Matrix.CreateRotationX(-MathHelper.PiOver2);
+            this.Initialize();
+        }
     }
-  }
 }
